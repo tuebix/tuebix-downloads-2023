9a964c9414d6723b14b0e9dd17e21bdb7c6d39cb
Michael Weiss <dev.primeos@gmail.com>
2023-07-02 21:16:48 +0200

    Update the naming scheme for 2023
    The current naming scheme isn't ideal as it lacks the authors name but
    we'll use the same scheme as on the website for now (the IDs are
    generated automatically based on the Pretalx data and our Python
    scripts). Maybe we can improve it a bit later but it'll probably be too
    difficult to keep it backwards compatible.

--- regen-index.sh
+++ regen-index.sh
@@ -11,11 +11,11 @@ echo -e "## Folien\n" >> index.md
 
 find -type f ! -empty | xargs git ls-files -z | tr '\0' '\n' | \
   grep -vE "README.md|index.md" | grep -E "\.(pdf|html|md|txt)$" | \
-  sed -E "s/tuebix-2023-(.*)/- [\1](tuebix-2023-\1)/" >> index.md
+  sed -E "s/(.*)/- [\1](\1)/" >> index.md
 
 echo -e "\n## Downloads\n" >> index.md
 # Section for everything that a web browser cannot display natively
 
 find -type f ! -empty | xargs git ls-files -z | tr '\0' '\n' | \
   grep -E "\.(cast|tar.gz|zip)$" | \
-  sed -E "s/tuebix-2023-(.*)/- [\1](tuebix-2023-\1)/" >> index.md
+  sed -E "s/(.*)/- [\1](\1)/" >> index.md
